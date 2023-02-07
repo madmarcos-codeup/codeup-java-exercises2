4981a28a582c816909275aca8c65d7eb3175de20
Docrob <mark.robinson@codeup.com>
2023-02-07 10:13:39 -0600

    first commit

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // the program starts executing here
+        System.out.println("Hello world");
+    }
+}
